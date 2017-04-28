a489f1756441c08301807f6eb4671e20ef3c1b0b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-04-29 02:26:52 +0800

    trying to call md5 lib

--- src/md5crash.c
+++ src/md5crash.c
@@ -3,7 +3,26 @@
 #include<string.h>
 #include<openssl/md5.h>
 
+void find(char* ans, int ans_len){
+  char* str = ans;
+  int length = ans_len;
+  unsigned char digest[16];
+  MD5_CTX md5;
+  MD5Init(&md5);
+  MD5Update(&md5, str, length);
+  MD5Final(digest, &md5);
+}
+
 int main(int argc, char ** argv){
-  printf("test\n");
+  printf("argc: %i\n",argc);
+  if(argc!=2){
+    fprintf(stderr, "Error: invalid argument.\nExample: %s <md5 hash to guess>\n", argv[0]);
+    return 1;
+  }
+  char* ans = argv[1];
+  printf("finding value for hash value: %s\n", ans);
+
+  find(ans, strlen(ans));
+
   return 0;
 }
